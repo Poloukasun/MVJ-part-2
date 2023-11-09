c501e19e2c598e1cdad5000f152f206db14f2c41
VLukyanov <vlad_lukyanov_88@icloud.com>
2023-11-09 14:42:56 -0500

    delete *

--- js/disconnect.js
+++
@@ -1,7 +0,0 @@
-import { ajaxRequest } from "./functions.js";
-
-$(document).ready(() => {
-    // $('#disconnect').on ("click", () => {
-    //     ajaxRequest("POST", "./server/logout.php", {});
-    // });
-});
--- server/reject_frienshiph.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if(isset($_POST)) {
-            echo json_encode('works');
-        }
-    }
-
-?>
